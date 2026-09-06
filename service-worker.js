f7573306b1edd071343a47f5bf9154eb91356d12
szjvhrkhs7-cmyk <szjvhrkhs7@privaterelay.appleid.com>
2026-09-06 14:41:03 +0300

    Build first Chronicles of the Seeker campaign notebook

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,28 @@
-const CACHE = "saldens-orders-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./menu.js",
-  "./manifest.webmanifest",
-  "./assets/saldens-burger.jpg",
-  "./assets/saldens-pizza.jpg"
-];
+const CACHE_NAME = 'seeker-chronicles-v1';
+const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))
-    )
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
   event.respondWith(
-    caches.match(event.request).then((cached) =>
-      cached || fetch(event.request).then((response) => {
+    fetch(event.request)
+      .then((response) => {
         const copy = response.clone();
-        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
         return response;
       })
-    )
+      .catch(() => caches.match(event.request))
   );
 });
