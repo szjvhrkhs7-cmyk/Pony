ba7f2062811046714f96ebac871d15ee0f0d142e
szjvhrkhs7-cmyk <szjvhrkhs7@privaterelay.appleid.com>
2026-09-06 15:02:46 +0300

    Refresh PWA cache for empty mobile-first notebook

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'seeker-chronicles-v1';
-const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
+const CACHE_NAME = 'seeker-chronicles-v2';
+const APP_SHELL = ['./', './index.html', './styles.css', './mobile.css', './app.js', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
